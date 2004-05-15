db6f7a6c64e3a18c877e2128c28800dd713e580a
unknown <unknown@36e7e03e-8437-11dd-8f23-d9c8888c2974>
2004-05-15 16:09:26 +0000

    This commit was manufactured by cvs2svn to create tag 'RELEASE-0_7_5'.
    git-svn-id: http://dircproxy.googlecode.com/svn/tags/RELEASE-0_7_5@323 36e7e03e-8437-11dd-8f23-d9c8888c2974

--- src/irc_server.c
+++ src/irc_server.c
@@ -5,7 +5,7 @@
  * irc_server.c
  *  - Handling of servers connected to the proxy
  * --
- * @(#) $Id: irc_server.c,v 1.22 2000/09/27 16:45:32 keybuk Exp $
+ * @(#) $Id: irc_server.c,v 1.22.2.2 2000/10/16 11:21:00 keybuk Exp $
  *
  * This file is distributed according to the GNU General Public
  * License.  For full details, read the top of 'main.c' or the
@@ -332,7 +332,7 @@ static int _ircserver_gotmsg(struct ircproxy *p, const char *str) {
 
     /* Restore the user mode */
     if (p->modes)
-      ircserver_send_command(p, "MODE", "+%s", p->modes);
+      ircserver_send_command(p, "MODE", "%s +%s", p->nickname, p->modes);
 
     /* Restore the away message */
     if (p->awaymessage) {
@@ -595,15 +595,17 @@ static int _ircserver_gotmsg(struct ircproxy *p, const char *str) {
       str = x_strdup(msg.params[1]);
       ircprot_stripctcp(str);
       if (strlen(str)) {
-        if (msg.src.username && msg.src.hostname) {
+        if (msg.src.type & IRC_USER) {
           char *tmp;
 
           tmp = x_sprintf("%s!%s@%s", msg.src.name, msg.src.username,
                           msg.src.hostname);
           irclog_msg(p, msg.params[0], tmp, "%s", str);
           free(tmp);
-        } else {
+        } else if (msg.src.type & IRC_SERVER) {
           irclog_msg(p, msg.params[0], msg.src.name, "%s", str);
+        } else {
+          irclog_msg(p, msg.params[0], p->servername, "%s", str);
         }
       }
       free(str);
@@ -619,15 +621,17 @@ static int _ircserver_gotmsg(struct ircproxy *p, const char *str) {
       str = x_strdup(msg.params[1]);
       ircprot_stripctcp(str);
       if (strlen(str)) {
-        if (msg.src.username && msg.src.hostname) {
+        if (msg.src.type & IRC_USER) {
           char *tmp;
 
           tmp = x_sprintf("%s!%s@%s", msg.src.name, msg.src.username,
                           msg.src.hostname);
-          irclog_notice(p, msg.params[0], tmp, str);
+          irclog_notice(p, msg.params[0], tmp, "%s", str);
           free(tmp);
+        } else if (msg.src.type & IRC_SERVER) {
+          irclog_notice(p, msg.params[0], msg.src.name, "%s", str);
         } else {
-          irclog_notice(p, msg.params[0], msg.src.name, str);
+          irclog_notice(p, msg.params[0], p->servername, "%s", str);
         }
       }
       free(str);
